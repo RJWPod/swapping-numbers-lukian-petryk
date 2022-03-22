05b4b199804d89346b776fea61671fca849a9347
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-03-22 09:51:42 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,27 +2,41 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("x")
     Number_1 += 1
     basic.showNumber(Number_1)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.TiltLeft, function () {
     basic.showString("x")
     Number_1 += -1
     basic.showNumber(Number_1)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("y")
     basic.showNumber(Number_2)
     basic.showString("x")
     basic.showNumber(Number_1)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("y")
     Number_2 += 1
     basic.showNumber(Number_2)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("x")
+    Number_1 = 0
+    basic.showNumber(Number_1)
+    basic.showString("y")
+    Number_2 = 0
+    basic.showNumber(Number_2)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.TiltRight, function () {
     basic.showString("y")
     Number_2 += -1
     basic.showNumber(Number_2)
+    basic.clearScreen()
 })
 let Number_2 = 0
 let Number_1 = 0
